7109a5dac29059d196a21d1b1981661d6ea9150a
gagankj <gnjhanjhi@gmail.com>
2025-01-17 17:38:27 +0530

    dotenv updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("mongodb+srv://Gagan:mpvbw7rUGYlCJHcT@cluster0.rmu1e.mongodb.net/")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
